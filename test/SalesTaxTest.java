be7180466fa625df905c860d92248eb9b18faf14
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-15 11:41:16 +0530

    added spec for SalesTax Class to calculate total cost for Nontaxable item

--- test/SalesTaxTest.java
+++ test/SalesTaxTest.java
@@ -9,7 +9,7 @@ public class SalesTaxTest {
 
 
     @Test
-    public void shouldReturnTotalCostOfItem(){
+    public void shouldReturnTotalCostOfTaxableItem(){
         HashMap<Item, Double> items = new HashMap<Item, Double>();
         Item tempItem = new Item("CD", 30.0, Values.TAXABLE, Values.IMPORTED);
         items.put(tempItem, 31.5);
@@ -20,6 +20,20 @@ public class SalesTaxTest {
 
 
         assertEquals("CD 34.65\n", salesTax.toString());
+    }
+
+    @Test
+    public void shouldReturnTotalCostOfNonTaxableItem(){
+        HashMap<Item, Double> items = new HashMap<Item, Double>();
+        Item tempItem = new Item("juice", 30.0, Values.NON_TAXABLE, Values.IMPORTED);
+        items.put(tempItem, 31.5);
+        SalesTax salesTax = new SalesTax(items);
+
+        salesTax.calculate();
+
+
+
+        assertEquals("juice 31.5\n", salesTax.toString());
     }
 
 }
