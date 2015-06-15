827eb6317891a5ad144617c1866da3da4118b9e3
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-15 11:26:47 +0530

    added spec for ImportedTax Class to calculate original cost for non imported item

--- test/ImportedTaxTest.java
+++ test/ImportedTaxTest.java
@@ -22,4 +22,18 @@ public class ImportedTaxTest {
         assertEquals("juice 31.5\n", importedTax.toString());
     }
 
+    @Test
+    public void shouldReturnOriginalCostOfItemWhichIsNotImported(){
+        ArrayList<Item> items = new ArrayList<Item>();
+        Item tempItem = new Item("juice", 30.0, Values.NON_TAXABLE, Values.NON_IMPORTED);
+        items.add(tempItem);
+        ImportedTax importedTax = new ImportedTax(items);
+
+        importedTax.calculate();
+
+
+
+        assertEquals("juice 30.0\n", importedTax.toString());
+    }
+
 }
