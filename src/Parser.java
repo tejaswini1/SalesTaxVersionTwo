4d4f97213df74daa2a22719567470dd0c30deeb8
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-15 11:55:22 +0530

    added spec for parser to create object of Item from given inputed string

---
+++ src/Parser.java
@@ -0,0 +1,25 @@
+import org.mockito.asm.tree.analysis.Value;
+
+public class Parser {
+
+    public Item parse(String input){
+        String[] temp = input.split(" ");
+        String name = temp[temp.length - 3];
+        double cost = Double.parseDouble(temp[temp.length - 1]);
+
+        if(input.contains("imported")){
+
+            return new Item(name, cost, isTaxable(name), Values.IMPORTED);}
+
+        else {
+
+            return new Item(name, cost, isTaxable(name), Values.IMPORTED);}
+    }
+
+    private boolean isTaxable(String name) {
+
+        if(name.equals("CD") || name.equals("perfume"))
+            return false;
+        return true;
+    }
+}
