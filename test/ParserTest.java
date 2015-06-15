fbf193aedd568f743a8ab1cb76a314350b70d379
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-15 12:00:48 +0530

    refactored

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -7,7 +7,7 @@ public class ParserTest {
     @Test
     public void shouldCreateAnItemObject(){
         Parser parser = new Parser();
-        
+
         Item item = parser.parse("1 juice at 30.0");
 
         assertEquals(new Item("juice", 30.0, Values.NON_TAXABLE, Values.NON_IMPORTED),item);
